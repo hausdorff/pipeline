499893e729bf2ab5f3199b02dc2602a42b5cc4c3
John <johnshew@twitter.com>
2016-03-12 10:45:35 -0800

    Reenabled and cleaned up

--- test/confirm-mocha-chai.ts
+++ test/confirm-mocha-chai.ts
@@ -1,7 +1,7 @@
 import chai = require('chai');
 var expect = chai.expect;
-/*
-describe('User Model Unit Tests:', () => {
+
+describe('Confirm Mocha and Chai are working', () => {
 
     describe('2 + 4', () => {
         it('should be 6', (done) => {
@@ -15,4 +15,3 @@ describe('User Model Unit Tests:', () => {
         });
     });
 });
-*/
