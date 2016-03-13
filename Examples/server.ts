630099ff4257c7489b7e29b16cd84cee3f5ca8f7
Alex Clemmer <clemmer.alexander@gmail.com>
2016-03-13 15:58:06 -0700

    Add comments to `server.ts`.

--- Examples/server.ts
+++ Examples/server.ts
@@ -7,6 +7,9 @@ import pipelineConfig = require('./pipelineConfig');
 var log = require('winston');
 log.level = 'error';
 
+// ----------------------------------------------------------------------------
+// Start up the configuration server.
+// ----------------------------------------------------------------------------
 pipesConfigServer.listen(pipelineConfig.pipelineConfigServerPort);
 
 if (pipesConfigServer.server) { 
@@ -15,51 +18,83 @@ if (pipesConfigServer.server) {
     throw 'Configuration server not available.';
 }
 
+// ----------------------------------------------------------------------------
+// Start the pipeline, including two stages, `countStore` and `planStore`.
+// ----------------------------------------------------------------------------
 import startPipeline = require('./startPipeline');
 if (!startPipeline.ready()) { throw 'Pipeline is not ready'; }
 
+// Get pipeline pointed at configuration server at `http://localhost:9000`. The
+// configuration server will tell it things like the names of stages, so that
+// the `Pipeline` object can broker interactions between stages.
 var pipeline = pipes.createPipeline(pipelineConfig.pipelineConfigServerUrl.href);
 var restifySessions = new pipes.RestifySessionManager();
 
-// Start a vanilla restify server that send messages to a pipeline
-
+// ----------------------------------------------------------------------------
+// Start a frontdoor server.
+// ----------------------------------------------------------------------------
 var server = restify.createServer();
 
-function restifyServerHandler(pipeline: pipes.Pipeline, request: restify.Request, response: restify.Response, next: restify.Next) {
-    log.info('Got message from client with paramaters ' + JSON.stringify(request.params));
-    var operation = request.params.operation;
-    var session = restifySessions.add(request, response, next);
+function restifyServerHandler(pipeline: pipes.Pipeline,
+                              request: restify.Request,
+                              response: restify.Response, next: restify.Next) {
+    log.info('Got message from client with paramaters ' +
+             JSON.stringify(request.params));
+    let operation = request.params.operation;
+    let session = restifySessions.add(request, response, next);
 
     pipeline.send(pipelineConfig.planStoreStage,
         '/lookup/' + operation,
-        pipeline.merge(request.params, { operation: operation, session: session, initialNode: pipelineServer.myUrl.href }));
+        pipeline.merge(
+            request.params,
+            {
+                operation: operation,
+                session: session,
+                initialNode: pipelineServer.myUrl.href
+            }));
 }
 
-server.post('/api/:operation', (req, res, next) => restifyServerHandler(pipeline, req, res, next));
-server.get('/api/:operation', (req, res, next) => restifyServerHandler(pipeline, req, res, next));
+// Set up the front door's routes.
+server.post(
+    '/api/:operation',
+    (req, res, next) => restifyServerHandler(pipeline, req, res, next));
+server.get(
+    '/api/:operation',
+    (req, res, next) => restifyServerHandler(pipeline, req, res, next));
 
+// Start listening.
 server.listen(pipelineConfig.frontdoorRestPort);
 log.info("REST interface listening on", pipelineConfig.frontdoorRestPort);
 
-// Instantiate a pipeline server that will listen on /pipeline/result and send the result to the client.
-
+// ----------------------------------------------------------------------------
+// Create a pipeline server for the results.
+// ----------------------------------------------------------------------------
 var pipelineServer = pipeline.createServer('frontdoorStage');
 
-pipelineServer.process('/pipeline/result', (params, next) => {
-    log.info("Got result ", params);
-    var result = params && params["result"];
-    var sessionId = params && params["session"];
-    var session = sessionId && restifySessions.find(sessionId);
+// Set up a handler to process results.
+pipelineServer.process(
+    '/pipeline/result',
+    (params, next) => {
+        log.info("Got result ", params);
+        let result = params && params["result"];
+        let sessionId = params && params["session"];
+        let session = sessionId && restifySessions.find(sessionId);
 
-    if (!result || !session) { log.info('/pipeline/result called improperly'); return; }
+        if (!result || !session) {
+            log.info('/pipeline/result called improperly');
+            return;
+        }
 
-    log.info('Send response to HTTP client with body', params["result"]);
-    session.response.send(201, params["result"]);
-    session.next();
-    next();
-});
+        log.info('Send response to HTTP client with body', params["result"]);
+        session.response.send(201, params["result"]);
+        session.next();
+        next();
+    });
 
+// Start the server.
 pipelineServer.listen(pipelineConfig.frontdoorPort);
+
 log.info("Initial pipeline stage listening on", pipelineConfig.frontdoorPort);
 
+// Tell scripts consuming this file that we're read.
 export var ready = true;
