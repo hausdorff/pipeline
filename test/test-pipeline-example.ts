03b34dfc99790428abbaf7afa7edbe7cf7fabd22
Alex Clemmer <clemmer.alexander@gmail.com>
2016-03-12 17:19:50 -0800

    Transition from `console.log` to winston logging in tests

--- test/test-pipeline-example.ts
+++ test/test-pipeline-example.ts
@@ -2,11 +2,14 @@ import restify = require('restify');
 import chai = require('chai');
 import server = require('../Examples/server');
 
+var log = require('winston');
+log.level = 'error';
+
 var expect = chai.expect;
 var should = chai.should();
 
-while (!server.ready) { setTimeout(() => { console.log('Waiting.'); }, 0); }
-console.log('server is ready');
+while (!server.ready) { setTimeout(() => { log.info('Waiting.'); }, 0); }
+log.info('server is ready');
 var client = restify.createJsonClient({
     url: 'http://127.0.0.1:8000',
 
@@ -19,7 +22,7 @@ describe('Examples/server REST API', () => {
 
         it('should return hello', (done) => {
             client.get('/api/hello', (err, req, res, data) => {
-                console.log('callback for /api/hello', err, res.statusCode, 'with data', data);
+                log.info('callback for /api/hello', err, res.statusCode, 'with data', data);
                 expect(err).to.be.null;
                 expect(res.statusCode).to.equal(201);
                 expect(data).to.equal("Hello");
@@ -29,7 +32,7 @@ describe('Examples/server REST API', () => {
 
         it('should lookup a count and return the count', (done) => {
             client.get('/api/counter', (err, req, res, data) => {
-                console.log('callback for /api/counter', err, res.statusCode, 'with data', data);
+                log.info('callback for /api/counter', err, res.statusCode, 'with data', data);
                 expect(err).to.be.null;
                 expect(res.statusCode).to.equal(201);
                 expect(data).to.be.a('number');
@@ -39,7 +42,7 @@ describe('Examples/server REST API', () => {
 
         it('should do a simple calculation and return the result', (done) => {
             client.get('/api/simple', (err, req, res, data) => {
-                console.log('callback for /api/simple', err, res.statusCode, 'with data', data);
+                log.info('callback for /api/simple', err, res.statusCode, 'with data', data);
                 expect(err).to.be.null;
                 expect(res.statusCode).to.equal(201);
                 expect(data).to.be.a('string');
@@ -49,7 +52,7 @@ describe('Examples/server REST API', () => {
 
         it('should lookup a counter, do a simple calculation, and return the result', (done) => {
             client.get('/api/chained', (err, req, res, data) => {
-                console.log('callback for /api/chained', err, res.statusCode, 'with data', data);
+                log.info('callback for /api/chained', err, res.statusCode, 'with data', data);
                 expect(err).to.be.null;
                 expect(res.statusCode).to.equal(201);
                 expect(data).to.be.a('string');
