4f82d46fca2585705fb98054bd4ae462a59d1095
Alex Clemmer <clemmer.alexander@gmail.com>
2016-03-21 02:44:21 -0700

    Add simple programmatic Babel example

---
+++ test/test-babel.js
@@ -0,0 +1,63 @@
+import * as babylon from "babylon";
+import traverse from "babel-traverse";
+import * as t from "babel-types";
+import generate from "babel-generator";
+import template from "babel-template";
+
+//
+// EXAMPLE 1.
+//
+const code = `function square(n) {
+  return n * n;
+}`;
+
+// Parse the function.
+const ast = babylon.parse(code);
+
+// Change the symbol `n` -> `x`, using the `babel-traverse` API.
+traverse(ast, {
+  enter(path) {
+    if (
+      path.node.type === "Identifier" &&
+      path.node.name === "n"
+    ) {
+      path.node.name = "x";
+    }
+  }
+});
+
+// Change the symbol `n` -> `x`, using the `babel-types` API.
+traverse(ast, {
+  enter(path) {
+    if (t.isIdentifier(path.node, { name: "n" })) {
+      path.node.name = "x";
+    }
+  }
+});
+
+// Generate the code after the traversal.
+const generatedCode = generate(ast, null, code);
+
+console.log("Rewritten code:");
+console.log(generatedCode.code);
+console.log()
+
+
+//
+// EXAMPLE 2.
+//
+
+// Declare AST template.
+const buildRequire = template(`
+  var IMPORT_NAME = require(SOURCE);
+`);
+
+// Construct AST from template.
+const ast2 = buildRequire({
+  IMPORT_NAME: t.identifier("myModule"),
+  SOURCE: t.stringLiteral("my-module")
+});
+
+// Generate code.
+console.log("Templated AST:")
+console.log(generate(ast).code);
