85ea4b12ba402703838faf82eec90d0c4f6d226e
JohnShew <johnshew@twitter.com>
2016-03-24 12:51:59 -0700

    Updated to match conversations with Alex

--- Examples/SimpleDatabaseAndCachingApp/generated.ts
+++ Examples/SimpleDatabaseAndCachingApp/generated.ts
@@ -1,25 +1,42 @@
-import * as continuum from "../../src/core/Continuum";
+import * as continua from "../../src/core/Continuum";
 import * as stage from "../../src/core/Stage";
-import * as cache from "./Cache/CacheStage";
-import * as db from "./Database/DbStage";
-import * as processing from "./Processing/ProcessingStage";
 import * as sb from "../../src/core/ServiceBroker";
 
 var log = require('winston');
 log.level = 'error';
 
+export * from "../../src/core/Stage";
 
 export type CBContinuation<T> = (continuum: Continuum, stage: T, params: any) => void;
 
-export class Continuum extends continuum.ContinuumBase {
-    public cacheStage: cache.CacheStageInterface = null;
-    public dbStage: db.DbStageInterface = null;
-    public processingStage: processing.ProcessingStageInterface = null;
+export interface CacheStageInterface extends stage.Stage {
+    has: (k: string) => boolean;
+    get: (k: string) => string;
+    set: (k: string, v: string) => boolean;
+    getDataAndProcess: (continuum: Continuum, cs: CacheStageInterface, params: any) => void
+
+}
+
+export interface DbStageInterface extends stage.Stage {
+    getThing(k: string): string
+    cacheAndProcessData: (continuum: Continuum, dbs: DbStageInterface, params: any) => void;
+}
+
+export interface ProcessingStageInterface extends stage.Stage {
+    thingWasProcessed: boolean;
+    doThing: (v: string) => void;
+    processData: (continuum: Continuum, pss: ProcessingStageInterface, params: any) => void;
+}
+
+export class Continuum extends continua.ContinuumBase {
+    public cacheStage: CacheStageInterface = null;
+    public dbStage: DbStageInterface = null;
+    public processingStage: ProcessingStageInterface = null;
 
     public log = log;
 
     public forward<T extends stage.Stage>(toStage: T, params: any,
-                                          c: CBContinuation<T>) { 
+        c: CBContinuation<T>) {
         return this.forwardWithSelectorImplementation<T, Continuum>(
             toStage,
             params,
@@ -28,8 +45,8 @@ export class Continuum extends continuum.ContinuumBase {
     }
 
     public forwardWithSelector<T extends stage.Stage>(toStage: T, params: any,
-                                                      s: sb.Selector,
-                                                      c: CBContinuation<T>) { 
+        s: sb.Selector,
+        c: CBContinuation<T>) {
         return this.forwardWithSelectorImplementation<T, Continuum>(
             toStage,
             params,
